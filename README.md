baa0d3fa3154a96669e23ac0fde1d12ec066b79f
Raphael Neuenschwander <neuenschwander.raphael@gmail.com>
2015-09-28 10:10:33 +0200

    feat: Animation of a row
    Animation of a row using spacer group technique

---
+++ README.md
@@ -0,0 +1,13 @@
+#WatchOS 2.0#
+
+![Platform](https://img.shields.io/badge/platform-watchOS-ff69b4.svg)
+![License](https://img.shields.io/badge/license-MIT-blue.svg)
+
+##Why##
+1. To exlore the new capabilities of WatchOS 2.0
+
+##License##
+This code is distributed under the terms and conditions of the MIT license.
+
+By: Raphael Neuenschwander
+- neuenschwander.raphael@gmail.com
---
+++ WatchOS 2.0 Watch App Extension/RowAnimationInterfaceController.swift
@@ -0,0 +1,52 @@
+//
+//  RowAnimationInterfaceController.swift
+//  WatchOS 2.0
+//
+//  Created by Raphael Neuenschwander on 28.09.15.
+//  Copyright © 2015 Raphael Neuenschwander. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class RowAnimationInterfaceController: WKInterfaceController {
+
+    @IBOutlet var imageNearMe: WKInterfaceImage!
+    @IBOutlet var spacerGroup: WKInterfaceGroup!
+
+    @IBOutlet var textLabel: WKInterfaceLabel!
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        imageNearMe.setImage(UIImage(named: "Near Me Filled"))
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        imageNearMe.setAlpha(0.0)
+        textLabel.setAlpha(0.0)
+    
+    }
+    
+    override func didAppear() {
+        super.didAppear()
+        // Set the width of the spacer group to 0, which will make this group disappear and affect the label layout (comes from the right)
+        animateWithDuration(0.3) { () -> Void in
+            self.imageNearMe.setAlpha(1.0)
+            self.textLabel.setAlpha(1.0)
+            self.spacerGroup.setWidth(0.0)
+        }
+
+        
+    }
+
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
