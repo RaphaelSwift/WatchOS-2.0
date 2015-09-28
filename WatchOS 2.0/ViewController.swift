b8a66a82642cb5b2bbda2bbe3e5b4d15cac77020
Raphael Neuenschwander <neuenschwander.raphael@gmail.com>
2015-09-28 08:50:33 +0200

    feat: Initialized project, first commit

---
+++ WatchOS 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchOS 2.0
+//
+//  Created by Raphael Neuenschwander on 28.09.15.
+//  Copyright © 2015 Raphael Neuenschwander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
