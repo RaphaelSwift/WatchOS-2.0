734c44f0182a6dae536b54972937927776607aca
Raphael Neuenschwander <neuenschwander.raphael@gmail.com>
2015-10-01 14:29:16 +0200

    feat: List Picker with random color

--- WatchOS 2.0 Watch App Extension/ListPickerInterfaceController.swift
+++ WatchOS 2.0 Watch App Extension/ListPickerInterfaceController.swift
@@ -36,12 +36,41 @@ class ListPickerInterfaceController: WKInterfaceController {
         for i in 1...10 {
             let item = WKPickerItem()
             item.title = "item" + "\(i)"
-            item.accessoryImage = WKImage(image: UIImage(named: "Success")!)
+            item.accessoryImage = WKImage(imageName: "Success")
+            item.contentImage = WKImage(image: drawColoredRect())
             item.caption = "caption" + "\(i)"
             pickerItems.append(item)
         }
         listPicker.setItems(pickerItems)
     }
 
+    
+    func drawColoredRect() -> UIImage {
+        
+        let rect = CGRectMake(0, 0, 600, 50)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        CGContextSetFillColorWithColor(context, UIColor.random.CGColor)
+        CGContextFillRect(context, rect)
+        let randomColorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return randomColorImage
+    }
+}
 
+private extension UIColor {
+    class var random: UIColor {
+        switch arc4random() % 8 {
+        case 0: return UIColor.blueColor()
+        case 1: return UIColor.greenColor()
+        case 2: return UIColor.purpleColor()
+        case 3: return UIColor.redColor()
+        case 4: return UIColor.yellowColor()
+        case 5: return UIColor.orangeColor()
+        case 6: return UIColor.cyanColor()
+        case 7: return UIColor.lightGrayColor()
+        default: return UIColor.magentaColor()
+        }
+    }
 }
